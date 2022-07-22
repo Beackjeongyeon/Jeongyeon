131b4d897ab12f51d4965298856a0d46be8fdc65
Beackjeongyeon <qorwjddus03@naver.com>
2022-07-22 16:57:35 +0900

    마이페이지 css/헤더작업중

--- src/main/java/com/example/theatherproject/controller/MemberController.java
+++ src/main/java/com/example/theatherproject/controller/MemberController.java
@@ -1,12 +1,11 @@
 package com.example.theatherproject.controller;
+
 import com.example.theatherproject.dto.MemberDTO;
 import com.example.theatherproject.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -29,12 +28,14 @@ public class MemberController {
         memberService.save(memberDTO);
         return "/index";
     }
+
     //로그인 페이징 처리
     @GetMapping("/login-form")
     public String loginForm() {
 
         return "memberPages/login";
     }
+
     //로그인처리
     @PostMapping("/login")
     public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
@@ -49,29 +50,34 @@ public class MemberController {
 
 
     }
+
     //마이페이지 페이징처리
     @GetMapping("/My-page")
-    public String MyPage(){
+    public String MyPage() {
         return "memberPages/Mypage";
     }
+
     // 회원정보수정
     @GetMapping("correction")
-    public String Correction(){
-        return "myPages/correction";
+    public String Correction(HttpSession session, Model model) {
+        return "/memberPages/correction";
     }
+
     // 예약정보확인
     @GetMapping("/confirm")
-    public String Confirm(){
-        return "myPages/confirm";
+    public String Confirm(HttpSession session, Model model) {
+        return "/memberPages/confirm";
     }
+
     //나의문의내역
     @GetMapping("/question")
-    public String question(){
-        return "myPages/question";
+    public String question(HttpSession session, Model model) {
+        return "/memberPages/question";
     }
+
     //회원 탈퇴
     @GetMapping("/secession")
-    public String Secession(){
-        return "myPages/secession";
+    public String secession() {
+        return "/memberPages/secession";
     }
 }
